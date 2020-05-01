be9bd487f7ae57da60ce31713a309402038322b4
Marcus Snow <marcusjsnow1@gmail.com>
2020-05-01 08:46:22 -0400

    Addresses table setup with foreign key

--- marcus_sql.py
+++ marcus_sql.py
@@ -1,17 +1,12 @@
 import sqlite3
 
 
-if __name__ == "__main__":
-    print('hello')
+# ToDo
+# Add validation to inputs
+#
 
-    #Create a connection to the DB
-    conn = sqlite3.connect('phonebook.db')
-
-    #Create a cursor object to perform SQL
+def init_tables():
     cursor = conn.cursor()
-
-    #Create the phonebook table
-    #if it doesn't exist already
     cursor.execute("CREATE TABLE IF NOT EXISTS PhoneBook  ("
                    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                    "first_name VARCHAR NOT NULL,"
@@ -21,18 +16,43 @@ if __name__ == "__main__":
                    "email VARCHAR ) ")
 
 
+    #How do I set a foreign key?
+    cursor.execute("CREATE TABLE IF NOT EXISTS Addresses "
+                   "( id INTEGER PRIMARY KEY AUTOINCREMENT, book_id INTEGER, address TEXT, FOREIGN KEY(book_id) REFERENCES PhoneBook(id) )")
 
-    #Ask User for Input
+    print("Tables successfully created and/or exist already.")
+
+
+if __name__ == "__main__":
+    print('hello')
+
+    # Create a connection to the DB
+    conn = sqlite3.connect('phonebook.db')
+
+    # Create a cursor object to perform SQL
+    cursor = conn.cursor()
+
+    # Create the phonebook table
+    # if it doesn't exist already
+    init_tables()
+
+    # Ask User for Input
     print("What would you like to do?")
     print("0. Show Records")
     print("1. Insert Phonebook")
+    print("2. Edit Phonebook")
+    print("3. Delete Phonebook Entry")
+    print("4. Insert Address")
+    print("5. Edit Address")
+    print("6. Delete Address")
+    print("7. Delete Everything")
 
     user_input = input("Select your command: ")
     print(user_input)
 
     if user_input == "1":
         print('User has selected insert phonebook')
-        #Get Fields for creating phonebook
+        # Get Fields for creating phonebook
 
         first_name = input("Enter the first name: ")
         last_name = input("Enter the last name: ")
@@ -40,27 +60,15 @@ if __name__ == "__main__":
         age = input("Enter age: ")
         email = input("Enter email: ")
 
-        #Insert fields into Phonebook Table
+        # Insert fields into Phonebook Table
         cursor.execute('''
             INSERT INTO PHONEBOOK (first_name, last_name, phone, age, email )
             VALUES(? , ?, ?, ?, ?)
         ''',
-                       (first_name,last_name, phone, age, email )
-        )
+                       (first_name, last_name, phone, age, email)
+                       )
         conn.commit()
 
         print('Person successfully added')
-
-
-
-
-
-
-
-
-    print("Table succeessfully made")
-
-
-
 
 
